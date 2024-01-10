c5456491a74055c125eb2b1a935c17eba64e2001
tehnickge <71207932+tehnickge@users.noreply.github.com>
2024-01-10 16:14:14 +0300

    .

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d4b0704073f1064b37fa23f718bc8f029b0380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
